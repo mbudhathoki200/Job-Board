09d073b75ba9bec8c603057a8f9af2a167308c2f
Manish Budhathoki <64700505+mbudhathoki200@users.noreply.github.com>
2024-07-30 10:25:33 +0545

    Add font & complete job page UI

--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -5,7 +5,11 @@ const withMT = require("@material-tailwind/html/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ["Plus Jakarta Sans", "sans - serif"],
+      },
+    },
   },
   plugins: [],
 });
