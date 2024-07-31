7635ed2b1cd421464f968023906bf37d5e34dfdb
Manish Budhathoki <64700505+mbudhathoki200@users.noreply.github.com>
2024-07-31 21:45:54 +0545

    Apply job dynamic

--- frontEnd/src/pages/job_details/index.ts
+++ frontEnd/src/pages/job_details/index.ts
@@ -6,13 +6,6 @@ const jobDetailsSection = document.getElementById(
   "job_details",
 ) as HTMLDivElement;
 const modal = document.getElementById("modal") as HTMLDivElement;
-const modalCloseBtn = document.getElementById(
-  "btn--close-modal",
-) as HTMLButtonElement;
-
-modalCloseBtn.addEventListener("click", () => {
-  modal.classList.toggle("hidden");
-});
 
 window.onload = async () => {
   let params = new URL(document.location.toString()).searchParams;
@@ -32,6 +25,7 @@ window.onload = async () => {
 
 function renderJobPage(data: IJOB) {
   jobDetailsSection.innerHTML = "";
+  modal.innerHTML = "";
   const dateFormated = formatDate(data.postDate);
   jobDetailsSection.innerHTML = /*html*/ `<div class="flex w-[80%] gap-8">
         <div class="w-[60%]">
@@ -188,8 +182,51 @@ function renderJobPage(data: IJOB) {
         </div>
       </div>`;
   const applyBtn = document.getElementById("apply-btn") as HTMLButtonElement;
-  console.log(applyBtn);
+
+  modal.innerHTML = /*HTML*/ `<div
+        class="fixed inset-0 z-50 flex items-center justify-center w-full transition-all duration-500 bg-black rounded-lg shadow-2xl bg-opacity-35"
+      >
+        <div class="fixed w-[40%] rounded-lg bg-white shadow-lg">
+          <button
+            class="absolute flex items-center justify-center text-3xl font-bold cursor-pointer right-4 top-2 size-10"
+            id="btn--close-modal"
+          >
+            &times;
+          </button>
+
+          <div class="p-6 text-lg font-bold bg-white border-b-2 rounded-t-lg">
+            Apply to ${data.companyName}
+          </div>
+          <form class="flex flex-col gap-4 p-6" id="apply_form">
+            <p class="font-medium">Upload Resume</p>
+            <p class="">Be sure to include an updated resume *</p>
+            <input
+              class="w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-sm cursor-pointer bg-gray-50 file:mx-3 file:h-9 file:cursor-pointer file:rounded-md file:border-none file:bg-blue-700 file:px-3 file:text-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              aria-describedby="file_input_help"
+              id="file_input"
+              type="file"
+              required
+            />
+            <p class="mt-1 text-sm text-gray-600" id="file_input_help ">
+              PDF, PNG or JPG (MAX. 10 MB).
+            </p>
+            <button
+              type="submit"
+              class="mb-2 me-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium uppercase tracking-wider text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Apply
+            </button>
+          </form>
+        </div>
+      </div>`;
   applyBtn.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+  });
+  const modalCloseBtn = document.getElementById(
+    "btn--close-modal",
+  ) as HTMLButtonElement;
+
+  modalCloseBtn.addEventListener("click", () => {
     modal.classList.toggle("hidden");
   });
 }
