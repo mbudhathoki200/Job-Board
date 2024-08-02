9babde05e5e1cbdf898eb6364c4b7632c997e662
Manish Budhathoki <64700505+mbudhathoki200@users.noreply.github.com>
2024-08-02 07:04:49 +0545

    Add dashboard adminTs

---
+++ frontEnd/src/pages/common/admin.ts
@@ -0,0 +1,22 @@
+const logoutBtn = document.getElementById("logout-btn") as HTMLButtonElement;
+const dashboardHome = document.getElementById("dashboard") as HTMLButtonElement;
+const viewJobsBtn = document.getElementById("viewJobs") as HTMLButtonElement;
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.clear();
+  window.location.href = "http://localhost:5173/";
+});
+
+dashboardHome.addEventListener("click", () => {
+  const location = "http://localhost:5173/src/pages/admin_dashboard/index.html";
+  if (window.location.href != location) {
+    window.location.href = location;
+  }
+});
+
+viewJobsBtn.addEventListener("click", () => {
+  const location = "http://localhost:5173/src/pages/admin_viewJobs/index.html";
+  if (window.location.href != location) {
+    window.location.href = location;
+  }
+});
